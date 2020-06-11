fb6490afe11b1a2df33b28c479eb886fc646787c
sophusstein <sophusstein@gmail.com>
2020-06-11 20:59:48 +0530

    created db

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+	Type = models.CharField(max_length =128, unique = True )
+
+	# def __unicode__(self):
+ #        return self.name
+
+
+class Symptoms(models.Model):
+	Plant =  models.ForeignKey(Plant,
+				on_delete=models.CASCADE,
+				)
+	Symptom =  models.CharField(max_length =256)      
+	PrevMeasure = models.CharField(max_length = 512)
+
+	# def __unicode__(self):
+ #        return self.name
